3b777a50218b3338596a9b868b986ccf0f667c44
SWAPNIL1702 <swapnil17190299@gmail.com>
2023-11-29 16:58:15 +0530

    file is added

--- main.js
+++ main.js
@@ -5,71 +5,89 @@ function Student(name, roll_no, class_name, section, marks_of_5_subjects) {
     this.class_name = class_name;
     this.section = section;
     this.marks_of_5_subjects = marks_of_5_subjects;
+
+    this.printReportCard = function () {
+      console.log("+--------------------+");
+      console.log("|     REPORT CARD    |");
+      console.log("+--------------------+");
+      console.log(`| Name     - ${this.name}     |`);
+      console.log(`| Roll no. - ${this.roll_no}     |`);
+      console.log(`| Class    - ${this.class_name}       |`);
+      console.log(`| Section  - ${this.section}       |`);
+      console.log(`| Maths   - ${this.marks_of_5_subjects.maths}       |`);
+      console.log(`| Science - ${this.marks_of_5_subjects.science}       |`);
+      console.log(`| English - ${this.marks_of_5_subjects.english}       |`);
+      console.log(`| Hindi - ${this.marks_of_5_subjects.hindi}         |`);
+      console.log(`| S.Sc    - ${this.marks_of_5_subjects.social_science}       |`);
+      console.log(`| Percentage -${percentage(this.marks_of_5_subjects)}    |`);
+      console.log("+--------------------+");
+    }
   
     // Writing the Function to print the top three subjects
     this.printTop3Subjects = function () {
-      // Sort subjects based on marks
-      const sortedSubjects = Object.entries(this.marks_of_5_subjects)
-        .sort((a, b) => b[1] - a[1])
-        .slice(0, 3);
+      printTop3Subjects(this.marks_of_5_subjects)
+    }
   
       // To Print top three subjects
-      console.log("Top 3 Subjects:");
+      function top3Subjects(marks){
+        console.log("Top 3 Subjects:");
+
+        const sortedSubjects = Object.keys(marks).sort(function (a,b) {
+          return marks[b] - marks[a];
+        });
+
+        for (let i = 0; i < 3; i++) {
+          const subject = sortedSubjects[i];
+          const subjectMarks = marks[Subjects];
+          console.log(`| ${i+1}. ${subject} - ${subjectMarks} |`);
+        }
+      }
+
+
+      
       sortedSubjects.forEach(([subject, marks]) => {
         console.log(`${subject}: ${marks}`);
       });
     };
-  
-    // Function to print the report card
-    this.printReportCard = function () {
-      // Calculate percentage
-      const totalMarks = Object.values(this.marks_of_5_subjects).reduce((sum, marks) => sum + marks, 0);
-      const percentage = (totalMarks / 500) * 100;
-  
-      // To Print report card
-      console.log("+--------------------+");
-      console.log("|     REPORT CARD    |");
-      console.log("+--------------------+");
-      console.log(`| Name     - ${this.name}`);
-      console.log(`| Roll no. - ${this.roll_no}`);
-      console.log(`| Class    - ${this.class_name}`);
-      console.log(`| Section  - ${this.section}`);
-      
-      // To Print marks of each subject
-      Object.entries(this.marks_of_5_subjects).forEach(([subject, marks]) => {
-        console.log(`| ${subject.charAt(0).toUpperCase() + subject.slice(1)}  - ${marks}`);
-      });
-  
-      console.log(`| Percentage - ${percentage.toFixed(1)} %`);
-      console.log("+--------------------+");
-    };
-  }
+    
+      // Function to Calculate the percentage
+      function percentage(obj) {
+        let totalmarks = 0;
+        let obtainedmarks = 0;
 
-  // An Example usage
-const studentDetails = {
-    name: "Huzaifa",
-    roll_no: 16,
-    class_name: "X",
-    section: "A",
-    marks_of_5_subjects: {
-      science: 73,
-      maths: 75,
-      social_science: 79,
-      english: 80,
-      hindi: 67
-    }
-  };
+        for (let subject in obj) {
+          if (obj.hasownProperty(subject)){
+            totalmarks += 100;
+            obtainedmarks += obj[subject];
+          }
+        }
+
+        let percentage = (obtainedmarks / totalmarks) * 100;
+        return percentage.toFixed(2);
+      }
   
-  const huzaifa = new Student(
-    studentDetails.name,
-    studentDetails.roll_no,
-    studentDetails.class_name,
-    studentDetails.section,
-    studentDetails.marks_of_5_subjects
-  );
-  
-  // To Print top three subjects
-  huzaifa.printTop3Subjects();
-  
-  // To Print report card
-  huzaifa.printReportCard();
+      // Student Details
+
+      let Student_1 = new Student ("Anthony", "7", "8th", "B",{
+        maths: 92,
+        science: 88,
+        english: 91,
+        hindi: 85,
+        social_science: 82
+      });
+
+      let Student_2 = new Student ("Steve", "9", "9th", "C", {
+        maths: 88,
+        science: 90,
+        english: 85,
+        hindi: 87,
+        social_science: 85
+      });
+
+      Student_1.printReportCard()
+      Student_1.top3Subjects()
+
+      Student_2.printReportCard()
+      Student_2.top3Subjects()
+      
+      
